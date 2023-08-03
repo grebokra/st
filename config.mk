0c51062fcca856a2d371bd027eab0cc672a2fe48
Georgy Melnikov <gmelnikov@hotmail.com>
2023-08-03 22:00:34 +0300

    customize st + change CFLAGS

--- config.mk
+++ config.mk
@@ -3,12 +3,14 @@ VERSION = 0.9
 
 # Customize below to fit your system
 
+CFLAGS=-O
+
 # paths
 PREFIX = /usr/local
 MANPREFIX = $(PREFIX)/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+X11INC = /usr/include/X11
+X11LIB = /usr/lib/X11
 
 PKG_CONFIG = pkg-config
 
